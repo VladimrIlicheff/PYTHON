d4289ebb8905f9598a6533682a06f6ad9f2b4d8b
Vladimir <user@MAK.local>
2022-12-22 20:09:24 +0700

    abrakadabra

--- DZ3.py
+++ DZ3.py
@@ -4,3 +4,13 @@
 # out
 # [2.0, 2.25, 2.37, 2.441, 2.488, 2.522]
 # 14.071
+
+num_list = []
+sum = 0
+n = int (input ('Введите длину списка '))
+for n in range (1, n+1):
+   num = (1 + 1/n) ** n 
+   num_list.append (round(num,3))
+   sum = sum + num 
+print (num_list)
+print (round(sum))
