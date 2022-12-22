1aed999fa8c9d908a669eceaa44bdd4bf0328d16
Vladimir <user@MAK.local>
2022-12-22 20:40:57 +0700

    пристрелить методистов! дольше с условиями разбирались...

--- DZ4.py
+++ DZ4.py
@@ -13,6 +13,19 @@
 # -> [-4, -3, -2, -1, 0, 1, 2, 3, 4]
 # -> There are no values for these indexes!
 
+num = int(input('Введите число: '))
+new_list = list(range(-num, num+1))
+print(new_list)
+a = int(input('Введите № 1го числа: '))
+b = int(input('Введите № 2го числа: '))
+if a <= ((num*2)+1) and b <=((num*2)+1):
+   a = new_list[a-1]
+   b = new_list[b-1]
+   print(a * b)
+else: 
+    print("There are no values for these indexes!")
+
+     
 
 
 
