1c31b2d652c744fd91d5d2223c9cbf0a772a20b4
Vladimir <user@MAK.local>
2023-01-10 20:03:47 +0700

    выводит в столбик

--- dz_3.py
+++ dz_3.py
@@ -6,8 +6,15 @@
 # in 10 out [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]   [6, 2, 3, 0, 9]
 
 import random 
-number = int(input("Ведите длинну последовательности:  "))
+number = int(input("Введите длинну последовательности:  "))
 list_source = [random.randrange(1, 15, 1) for i in range(number)]
 print (list_source)
+def get_unique(list_source):
+   list_final = []
+   for x in list_source:
+      if x not in list_final:
+        list_final.append(x)
+   for x in list_final:
+      print(x)
+get_unique(list_source)
 
-list_source(i)==
--- dz_proba.py
+++ dz_proba.py
@@ -22,27 +22,37 @@
 # с помощью дополнительных библиотек python. Запросите значения А, В, С 3 раза.
 # Уравнения и корни запишите в файл.
 
-from math import sqrt
-def discr(a, b, c):
-    D = b**2-4*a*c
-    with open("diskr.txt", "a", encoding="utf-8") as file: # открытие файла, в скобках - название файла, способ записи, кодировка, за скобками маркер и закрытие файла
-        file.write(f"{a}x² + {b}x + {c} = 0\n")
-        if D > 0:
-            x1 = (-b+sqrt(D))/(2*a)
-            x2 = (-b-sqrt(D))/(2*a)
-            file.write(f"{x1}, {x2}\n")
-        elif D == 0:
-            x = -b/(2*a)
-            file.write(f"{x}\n")
-        else:
-            file.write("Нет корней\n")
-discr(2, 3, 1)
-# 3. На вход программе подается число n.
-# Программа печатает численный треугольник.
-# Используем вложенные циклы.
-number = int(input())
-for i in range(1, number+1):
-    for k in range(i):
-      print(i, end="")
-print()
+# from math import sqrt
+# def discr(a, b, c):
+#     D = b**2-4*a*c
+#     with open("diskr.txt", "a", encoding="utf-8") as file: # открытие файла, в скобках - название файла, способ записи, кодировка, за скобками маркер и закрытие файла
+#         file.write(f"{a}x² + {b}x + {c} = 0\n")
+#         if D > 0:
+#             x1 = (-b+sqrt(D))/(2*a)
+#             x2 = (-b-sqrt(D))/(2*a)
+#             file.write(f"{x1}, {x2}\n")
+#         elif D == 0:
+#             x = -b/(2*a)
+#             file.write(f"{x}\n")
+#         else:
+#             file.write("Нет корней\n")
+# discr(2, 3, 1)
+# # 3. На вход программе подается число n.
+# # Программа печатает численный треугольник.
+# # Используем вложенные циклы.
+# number = int(input())
+# for i in range(1, number+1):
+#     for k in range(i):
+#       print(i, end="")
+# print()
 
+my_list = ['Python', 'C++', 'C#', 'Java', "Java", 'C++']
+
+
+my_set = set(my_list)
+New_List=list(my_set)
+
+print("Уникальным значениями в списке {0} являются".format(my_list))
+
+for i in New_List:
+  print(i)
