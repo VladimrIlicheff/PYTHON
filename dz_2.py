bb41d83991c400976e509e5b3302a73c7e710c12
Vladimir <user@MAK.local>
2023-01-12 20:54:53 +0700

    со скрипом

--- dz_2.py
+++ dz_2.py
@@ -4,3 +4,14 @@
 # in 54 out [2, 3, 3, 3]
 # in 9990 out [2, 3, 3, 3, 5, 37]
 # in 650 out [2, 5, 5, 13]
+
+num = int(input("Введите любое число:  "))
+count = 2
+my_list = []
+while num > 1:
+    if num % count == 0:
+        my_list.append(count)
+        num = num / count
+    else: 
+        count += 1
+print (my_list)
